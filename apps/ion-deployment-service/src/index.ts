f81cf4bc2a0d31655a95ec2507b55dd2d2f42b49
Tejas <tejas.teju02@gmail.com>
2026-03-22 15:30:13 +0530

    introdce all serive need to run.

---
+++ apps/ion-deployment-service/src/index.ts
@@ -0,0 +1,7 @@
+/*
+    1. Get the build ID from the QUEUE.
+    2. Based on the build ID pull the project from the S3.
+    3. Build the downloaded project (contanarize it).
+    4. The final built output final push to the S3 again (only dist files).
+*/
+
---
+++ apps/ion-request-service/src/index.ts
@@ -0,0 +1,6 @@
+/*
+    1. Here user will hit this to get the HTML that need to service in the internet.
+    2. get the files from the S3 based on the id and the filepath.
+    3. Return that file to the client in the formate of html to serve in the browser.
+*/
+
