9e716b9b2d50407ae771cf8c618d506eec415670
Tejas-pr <Tejas.teju02@gmail.com>
2026-05-27 21:56:06 +0530

    feat: initialize ion-db package with Prisma schema, client configuration, and database utility scripts

--- packages/ion-db/src/client.ts
+++ packages/ion-db/src/client.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -25,4 +25,4 @@ export const prisma =
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export * from "@prisma/client";
+export * from "./generated/client";
